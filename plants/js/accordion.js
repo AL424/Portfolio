e5e7293bf2d171d0c88d93549b67cff95be757b5
AL424 <algr5113@gmail.com>
2023-11-28 17:30:28 +0300

    refactor

--- plants/js/accordion.js
+++ plants/js/accordion.js
@@ -3,14 +3,14 @@ const arrows = document.querySelectorAll('.arrow-background');
 
 // функция закрывает все карточки и открывает заданную
 const openCard = (index) => {
-    cards.forEach(card => card.classList.remove('accordion__item_open'));
-    cards[index].classList.add('accordion__item_open');
+  cards.forEach(card => card.classList.remove('accordion__item_open'));
+  cards[index].classList.add('accordion__item_open');
 }
 
 //при клике на стрелку если карточка открыта, закрывает её, если нет выполняет функцию открытия
 arrows.forEach((arrow, index) => {
-    arrow.addEventListener('click', () => {
-        if (cards[index].classList.contains('accordion__item_open')) cards[index].classList.remove('accordion__item_open');
-        else openCard(index);
-    });
+  arrow.addEventListener('click', () => {
+    if (cards[index].classList.contains('accordion__item_open')) cards[index].classList.remove('accordion__item_open');
+    else openCard(index);
+  });
 })
--- plants/js/burger.js
+++ plants/js/burger.js
@@ -1,35 +1,30 @@
 const iconMenu = document.getElementById('menu__icon');
 const menu = document.getElementById('menu');
-const body = document.body
-
-//console.log(iconMenu);
-//console.log(menu);
+const body = document.body;
 
 //открытие-закрытие меню при нажатии на иконку
 iconMenu.addEventListener('click', function(e) {
-    menu.classList.toggle('menu_active');
-    iconMenu.classList.toggle('menu__icon_active')
-    body.classList.toggle('noscroll')
+  menu.classList.toggle('menu_active');
+  iconMenu.classList.toggle('menu__icon_active')
+  body.classList.toggle('noscroll')
 });
 
 //закрытие меню при нажатии на ссылку
 const linksMenu = document.querySelectorAll('.menu__link'); //массив ссылок меню
 
-//console.log(linksMenu);
-
 function closeOnClick() {
-    menu.classList.remove('menu_active');
-    iconMenu.classList.remove('menu__icon_active');
-    body.classList.remove('noscroll');
+  menu.classList.remove('menu_active');
+  iconMenu.classList.remove('menu__icon_active');
+  body.classList.remove('noscroll');
 };
 
 linksMenu.forEach((linkMenu) => { //используя этот метод добавляю функцию к каждому элкменту массива
-    linkMenu.addEventListener('click', closeOnClick);
+  linkMenu.addEventListener('click', closeOnClick);
 });
 
 //закрытие при нажатии не по меню
 document.addEventListener('click', function(event) {
-    if (!menu.contains(event.target) && !iconMenu.contains(event.target) && menu.classList.contains('menu_active')) {
-        closeOnClick();
-    };
+  if (!menu.contains(event.target) && !iconMenu.contains(event.target) && menu.classList.contains('menu_active')) {
+    closeOnClick();
+  };
 });
